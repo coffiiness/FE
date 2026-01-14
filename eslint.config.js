e9626c25902c00535c695d20c393e9d5f6793626
Apeirogon99 <apeirogon99@gmail.com>
2026-01-14 16:23:47 +0900

    ci(project): eslint config 추가

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+
+export default [
+    {
+        ignores: ['dist/**', 'node_modules/**']
+    },
+    js.configs.recommended,
+    ...pluginVue.configs['flat/essential'],
+    {
+        files: ['**/*.{js,mjs,cjs,vue}'],
+        rules: {
+            'no-unused-vars': 'warn',
+            'vue/multi-word-component-names': 'off'
+        }
+    }
+]
