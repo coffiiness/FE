15645f34aad902df6e775736d607f4f612795bc5
haengguk <guddnrb14@naver.com>
2026-02-10 12:01:11 +0900

    fix: [FE] index.js 충돌 해결 (#7)

--- src/router/index.js
+++ src/router/index.js
@@ -38,6 +38,21 @@ const routes = [
         component: () => import('@/views/RecruitmentView.vue')
       },
       {
+        path: 'recruitment/applicants',
+        name: 'ApplicantList',
+        component: () => import('@/views/ApplicantListView.vue')
+      },
+      {
+        path: 'recruitment/templates',
+        name: 'ApplicationTemplateList',
+        component: () => import('@/views/ApplicationTemplateListView.vue')
+      },
+      {
+        path: 'recruitment/templates/create',
+        name: 'ApplicationTemplateCreate',
+        component: () => import('@/views/ApplicationTemplateCreateView.vue')
+      },
+      {
         path: 'meeting-rooms',
         name: 'MeetingRooms',
         component: () => import('@/views/DashboardView.vue') // 임시 연결
